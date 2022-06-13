958e472c296c4ee016e2b36a779cdfe0f2755e08
Morgan Berry <morganlyn.berry@gmail.com>
2022-06-13 08:02:22 -0600

    Add eventListeners and querySelectors as well as rock, paper, scissors in HTML/CSS

--- main.js
+++ main.js
@@ -1,14 +1,20 @@
 //QuerySelectors:
-var classicGame = document.querySelector()("rules-box");
-var challengeGame =document.querySelector()("rules-box-two");
-var changeGameButton = document.querySelector("change-game-button");
+var classicGame = document.querySelector()(".rules-box");
+var challengeGame =document.querySelector()(".rules-box-two");
+var changeGameButton = document.querySelector(".change-game-button");
+var rockButton = document.querySelector("#rock");
+var paperButton = document.querySelector("#paper");
+var scissorsButton = document.querySelector("#scissors");
 
 
 //Event Listeners:
 window.addEventListener("load", displayPage);
 classicGame.addEventListener('click', playClassicGame);
 challengeGame.addEventListener('click', playChallengeGame);
-changeGameButton.addEventListener('click', mainPage);
+changeGameButton.addEventListener('click', changeGameButton);
+rockButton.addEventListener('click')
+paperButton.addEventListener('click')
+scissorsButton.addEventListener('click')
 
 
 //Global variables:
@@ -24,12 +30,27 @@ function displayPage() {
 
 playClassicGame() {
 this.type = "Classic";
+classicGame.classList.add("hidden");
+challengeGame.classList.add("hidden");
+rockButton.classList.remove("hidden");
+paperButton.classList.remove("hidden");
+scissorsButton.classList.remove("hidden");
+
 }
 
 playChallengeGame() {
-
+classicGame.classList.add("hidden");
+challengeGame.classList.add("hidden");
+rockButton.classList.remove("hidden");
+paperButton.classList.remove("hidden");
+scissorsButton.classList.remove("hidden");
 }
 
-mainPage() {
+changeGameButton() {
+classicGame.classList.remove("hidden");
+challengeGame.classList.remove("hidden");
+rockButton.classList.add("hidden");
+paperButton.classList.add("hidden");
+scissorsButton.classList.add("hidden");
 
 }
